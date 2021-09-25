ae87c04ccf7d060348704383c6beb88f44c51b0e
Fikcup <rhys.n.wright@gmail.com>
2021-09-24 22:00:45 -0700

    refactor : fixed curly braces to follow best practices

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,29 +7,25 @@ var grabButton = document.querySelectorAll("button");
 var answerArray = [];
 var remainingLetters;
 
-function guessesDisplay()
-{
+function guessesDisplay() {
     var outputGuesses = document.getElementById("guessesRemaining");
 
     outputGuesses.innerHTML = guessesRemaining;
 }
 
-function chooseWord()
-{
+function chooseWord() {
     // Chooses a random integer that coincides with words[index]
     wordsIndex = Math.floor(Math.random() * (words.length - 1));
     word = words[wordsIndex];
     displayWord();
 }
 
-function displayWord()
-{
+function displayWord() {
     remainingLetters = word.length;
     var output = document.getElementById("guessWord");
 
     // Initializes word displayed as underscores
-    for (let i = 0; i < word.length; i++)
-    {
+    for (let i = 0; i < word.length; i++) {
         answerArray[i] = "_";
     }
 
@@ -37,58 +33,45 @@ function displayWord()
     output.innerHTML = answerArray;
 }
 
-function updateDisplayWord()
-{
+function updateDisplayWord() {
     var output = document.getElementById("guessWord");
     answerArray.join("");
     output.innerHTML = answerArray;
 
     // Win condition met when _ have all been replaced by letters
-    if (answerArray.includes('_') == false)
-    {
+    if (answerArray.includes('_') !== true) {
         alert("You win! The word was " + words[wordsIndex] + ".");
     }
 }
 
-function incorrectGuesses()
-{
+function incorrectGuesses() {
     // Half the word length rounded down equals number of incorrect guesses
     guessesRemaining = Math.floor(words[wordsIndex].length / 2);
     console.log(guessesRemaining);
 }
 
-function checkLetter()
-{
+function checkLetter() {
     letter = this.value;    
 
     // If word contains the clicked letter
-   if (word.includes(letter))
-    {
+   if (word.includes(letter)) {
         // And the win condition hasn't been met
-        if (remainingLetters > 0)
-        {
+        if (remainingLetters > 0) {
             // Iterate through the word until letter's index is found
-            for (var j = 0; j < word.length; j++)
-            {
+            for (var j = 0; j < word.length; j++) {
                 // Replace underscore with letter
-                if (word[j] == letter)
-                {
+                if (word[j] == letter) {
                     answerArray[j] = letter;
                 }
             }
-
             updateDisplayWord();
         }
-
         remainingLetters--;
-    }
-    else
-    {
+    } else {
         guessesRemaining--;
         
         // If you have no guesses remaining lose condition has been met
-        if (guessesRemaining == 0) 
-        {
+        if (guessesRemaining == 0) {
             alert("You lose. The word was " + words[wordsIndex] + ". Refresh and try again");
         }
     }
